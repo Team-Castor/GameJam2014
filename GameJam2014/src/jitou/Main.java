ba3eabfb9481d4178c6c768f39448544b9e13186
pasquet <pasquet@pasquet-TECRA-R950>
2014-04-25 11:12:32 +0200

    ok

--- GameJam2014/src/jitou/Main.java
+++ GameJam2014/src/jitou/Main.java
@@ -1,10 +1,21 @@
 package jitou;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
 
 public class Main extends BasicGame {
-
-    public WindowGame()  {
+	private int x, y;
+	
+	
+    public Main()  {
         super("Lesson 1 :: WindowGame");
+        x = 0;
+    	y = 0;
+    
     }
 
     @Override
@@ -13,14 +24,24 @@ public class Main extends BasicGame {
 
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
+    	g.drawOval(x, y, 100,100);
+    	x=(x+1)%400;
+    	y=(y+1)%400;
+    	
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
+    	
     }
+    
+    
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SlickException {
 		// TODO Auto-generated method stub
+		AppGameContainer game  = new AppGameContainer(new Main(), 640, 480, false);
+		
+		game.start();
 
 	}
 
