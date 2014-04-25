ffaf339ab4890e314a421f10947d6905f59e0a7b
pasquet <pasquet@pasquet-TECRA-R950>
2014-04-25 11:01:54 +0200

    slick

---
+++ GameJam2014/src/jitou/Main.java
@@ -0,0 +1,27 @@
+package jitou;
+
+
+public class Main extends BasicGame {
+
+    public WindowGame()  {
+        super("Lesson 1 :: WindowGame");
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
