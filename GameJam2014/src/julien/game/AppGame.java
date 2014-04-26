897145c0080e14416b44c724fd3e3c358f0c1aa6
JulienNigon <julien.nigon@orange.fr>
2014-04-26 13:31:29 +0200

    TG!

--- GameJam2014/src/julien/game/AppGame.java
+++ GameJam2014/src/julien/game/AppGame.java
@@ -2,6 +2,7 @@ package julien.game;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.util.Log;
 
 public class AppGame extends AppGameContainer{
 	private static final String titreFenPrincipale = "Sous la surface";
@@ -9,8 +10,10 @@ public class AppGame extends AppGameContainer{
 	
 	public AppGame() throws SlickException{
 		super(new Game(titreFenPrincipale), 800, 600, false);
+		Log.setVerbose(false);
 		this.setSmoothDeltas(true);
-		this.setTargetFrameRate(60); 
+		this.setTargetFrameRate(60);
+		
 		this.start();
 	}
 	
