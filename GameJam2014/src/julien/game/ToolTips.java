c6671db03b146847c97709574df638fcf69bb4dd
Pasquet <le.jitou@gmail.com>
2014-04-27 18:19:24 +0200

    dfs

--- GameJam2014/src/julien/game/ToolTips.java
+++ GameJam2014/src/julien/game/ToolTips.java
@@ -1,5 +1,7 @@
 package julien.game;
 
+import java.awt.Font;
+
 import jitou.batiments.Batiment;
 import jitou.batiments.TypeBatiment;
 import jitou.global.BoardGame;
@@ -9,6 +11,7 @@ import julien.mechant.Carte;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 
 import de.matthiasmann.twl.Container;
 
@@ -19,11 +22,17 @@ public class ToolTips{
 	public String log;
 	int dx, dy;
 	double x, y;
+	public Font font;
+	public TrueTypeFont trueTypeFont;
 
 	public ToolTips( GameContainer container) {
 		this.dx = 40;
 		this.dy = container.getHeight()-60;
-		set("aucune");	
+		set("");	
+
+		font = new Font("Verdana", Font.BOLD, 20);
+		trueTypeFont = new TrueTypeFont(font, true);
+	    trueTypeFont.drawString(20.0f, 20.0f, "Slick displaying True Type Fonts", Color.green);
 
 	}
 
@@ -31,16 +40,18 @@ public class ToolTips{
 		x=dx;
 		y=dy;
 		this.log=log;
+
 	}
 
 
 	public void draw(GameContainer container) {
 		//System.out.println(x+"  "+y+"  "+log);
-		if(!log.isEmpty()) container.getGraphics().drawString(log,(int) x,(int) y);
+
+		if(!log.isEmpty())trueTypeFont.drawString((int)x, (int)y, log);// container.getGraphics().drawString(log,(int) x,(int) y);
 	}
 
 	public void update(int delta) {
-		y+=delta/20.0;
+		y+=delta/40.0;
 		if(y>dy+60) log="";
 	}
 
